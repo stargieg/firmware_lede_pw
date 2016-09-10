7d8bb22ae5d98ad9e41e992c5210f2f6ddef0ef1
Patrick Grimm <patrick@lunatiki.de>
2016-09-10 21:54:15 +0200

    feeds update

--- config.mk
+++ config.mk
@@ -10,4 +10,4 @@ TARGET=ar71xx_generic
 #TARGET=x86_64
 PACKAGES_LIST_DEFAULT=luci-lua-olsrv2 luci-ng-olsrv2
 LEDE_SRC=git://git.lede-project.org/source.git
-LEDE_COMMIT=533d16822c84bf33ff5d18b775dafd3ab4810b17
+LEDE_COMMIT=c1578d4fc9e53c9b80d858fc924adc8a66f5fce3
