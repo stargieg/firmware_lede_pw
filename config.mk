89e5e6f3a0cab92cd656566328cf53707671d500
patrick <patrick@lunatiki.de>
2017-10-08 20:38:52 +0200

    new default config

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
-#TARGET=ar71xx_generic
+TARGET=ar71xx_generic
 #TARGET=ar71xx_mikrotik
 #TARGET=ath25_generic
 #TARGET=brcm2708_bcm2708
@@ -9,10 +9,10 @@ SHELL:=$(shell which bash)
 #TARGET=mpc85xx_generic
 #TARGET=ramips_mt7620
 #TARGET=ramips_rt305x
-TARGET=ramips_mt76x8
+#TARGET=ramips_mt76x8
 #TARGET=x86_generic
 #TARGET=x86_geode
 #TARGET=x86_64
-PACKAGES_LIST_DEFAULT=bacnet
+PACKAGES_LIST_DEFAULT=default
 LEDE_SRC=git://git.lede-project.org/source.git
 LEDE_COMMIT=7d905f1e95539f9bc43fad4f2892d916ced546cb
