4217bfcc7b721bf661047476d1896ce251f657d3
patrick <patrick@lunatiki.de>
2017-10-16 15:50:27 +0200

    update turris-omnia.patch

--- config.mk
+++ config.mk
@@ -16,4 +16,4 @@ TARGET=ar71xx_generic
 #TARGET=x86_64
 PACKAGES_LIST_DEFAULT=default
 LEDE_SRC=git://git.lede-project.org/source.git
-LEDE_COMMIT=7d905f1e95539f9bc43fad4f2892d916ced546cb
+LEDE_COMMIT=699c6fcc314225f79156a26db418e15bbc6bf10f
